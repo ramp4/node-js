396bed25f75c9b1517a74d61c9e1df26e20c3a81
ramp4 <rampatv13@gmail.com>
2020-02-08 14:06:42 +0300

    add fs, implement consoleToJSON

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-const obj = require("./user");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-console.log(obj);
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8"
+    });
 
-obj.sayhello();
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "views", "index.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    } else if (req.url === "/about") {
+      fs.readFile(
+        path.join(__dirname, "views", "about.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    } else if (req.url === "/api/users") {
+      res.writeHead(200, { "Content-Type": "text/json" });
+
+      const users = [
+        { name: "Artsem", age: 21 },
+        { name: "Violetta", age: "20" }
+      ];
+
+      res.end(JSON.stringify(users));
+    }
+  } else if (req.method === "POST") {
+    const body = [];
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+    req.on("data", data => {
+      body.push(Buffer.from(data));
+    });
+
+    req.on("end", () => {
+      const message = body.toString().split("=")[1];
+
+      res.end(`<h1>Ваше сообщение: ${message}</h1>`);
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running...");
+});
